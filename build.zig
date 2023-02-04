9a28f73cc815e1e052195aeedf7c866eeba722cd
dweiller <4678790+dweiller@users.noreplay.github.com>
2023-02-04 15:54:41 +1100

    build: update for std.build changes

--- build.zig
+++ build.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const mode = b.standardOptimizeOption(.{});
 
-    const main_tests = b.addTest("src/test.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/test.zig" },
+        .optimize = mode,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
