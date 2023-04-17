499f3586434ab39c8a044ccc91e079048610b87b
dweiller <4678790+dweiller@users.noreplay.github.com>
2023-04-17 14:16:03 +1000

    feat: update for std.Build.addTest() change

--- build.zig
+++ build.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardOptimizeOption(.{});
 
     _ = b.addModule("strided-arrays", .{
@@ -13,9 +11,10 @@ pub fn build(b: *std.Build) void {
         .root_source_file = .{ .path = "src/test.zig" },
         .optimize = mode,
     });
+    const run_main_tests = b.addRunArtifact(main_tests);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&run_main_tests.step);
 
     b.default_step = test_step;
 }
