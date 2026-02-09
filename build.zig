08a01b512092ab0ebcf591c81306b5ea45031fff
dweiller <4678790+dweiller@users.noreply.github.com>
2026-02-09 14:25:33 +1100

    build: set minimum zig version to 0.14.0

--- build.zig
+++ build.zig
@@ -8,8 +8,11 @@ pub fn build(b: *std.Build) void {
     });
 
     const main_tests = b.addTest(.{
-        .root_source_file = b.path("src/test.zig"),
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/test.zig"),
+            .optimize = mode,
+            .target = b.graph.host,
+        }),
     });
     const run_main_tests = b.addRunArtifact(main_tests);
 
